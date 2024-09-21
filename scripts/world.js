133e0b950d4eeb96badcafc1a6c7b7e1799b8af4
Nimira43 <n.rabone72@aol.com>
2024-09-21 20:30:31 +0100

    Using the RNG to affect the way the terrain is generated and creating multiple possible worlds.

--- scripts/world.js
+++ scripts/world.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js'
+import { RNG } from './rng'
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 })
@@ -14,6 +15,7 @@ export class World extends THREE.Group {
   data = []
 
   params = {
+    seed: 0, 
     terrain: {
       scale: 30,
       magnitude: 0.5,
@@ -51,7 +53,8 @@ export class World extends THREE.Group {
   }
 
   generateTerrain() {
-    const simplex = new SimplexNoise()
+    const rng = new RNG(this.params.seed)      
+    const simplex = new SimplexNoise(rng)
     for (let x = 0; x < this.size.width; x++) {
       for (let z = 0; z < this.size.height; z++) {
         const value = simplex.noise(
