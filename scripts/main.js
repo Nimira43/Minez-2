92cefbe800d73fdae887afc41518247c764f4daa
Nimira43 <n.rabone72@aol.com>
2024-09-22 01:33:12 +0100

    Used textures to style blocks but there is still bugs and issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { World } from './world'
 import { createUI } from './ui'
@@ -11,6 +12,8 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setClearColor(0x87ceeb);
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 document.body.appendChild(renderer.domElement)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
@@ -26,14 +29,24 @@ world.generate()
 scene.add(world)
 
 function setUpLights() {
-  const light1 = new THREE.DirectionalLight()
-  light1.position.set(1, 1, 1)
-  scene.add(light1)
-  
-  const light2 = new THREE.DirectionalLight()
-  light2.position.set(-1, 1, -0.5)
-  scene.add(light2)
-  
+  const sun = new THREE.DirectionalLight()
+  sun.position.set(50, 50, 50)
+  sun.castShadow = true
+  sun.shadow.camera.left = -50
+  sun.shadow.camera.right = 50
+  sun.shadow.camera.bottom = -50
+  sun.shadow.camera.top = 50
+  sun.shadow.camera.near = 0.1
+  sun.shadow.camera.far = 100
+  sun.shadow.bias = -0.0005
+  // sun.shadow.mapSixe = new THREE.Vector2(256, 256)
+  sun.shadow.mapSixe = new THREE.Vector2(512, 512)
+  // sun.shadow.mapSixe = new THREE.Vector2(2048, 2048)
+  scene.add(sun)
+
+  const shadowHelper = new THREE.CameraHelper(sun.shadow.camera)
+  scene.add(shadowHelper)
+
   const ambient = new THREE.AmbientLight()
   ambient.intensity = 0.1
   scene.add(ambient)
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,5 +1,5 @@
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
-import { blocks } from './blocks'
+import { resources } from './blocks'
 
 export function createUI(world) {
   const gui = new GUI()
@@ -7,18 +7,22 @@ export function createUI(world) {
   gui.add(world.size, 'height', 8, 64, 1).name('Height')
 
   const terrainFolder = gui.addFolder('Terrain') 
-  terrainFolder.add(world.params, 'seed', 0, 10000).name('Seed')
+  terrainFolder.add(world.params, 'seed', 10, 10000).name('Seed')
   terrainFolder.add(world.params.terrain, 'scale', 10, 100).name('Scale')
   terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('Magnitude')
   terrainFolder.add(world.params.terrain, 'offset', 0, 1).name('Offset')
 
   const resourcesFolder = gui.addFolder('Resources')
-  resourcesFolder.add(blocks.stone, 'scarcity', 0, 1).name('Scarcity')
 
-  const scaleFolder = resourcesFolder.addFolder('Scale')
-  scaleFolder.add(blocks.stone.scale, 'x', 10, 100).name('X Scale')
-  scaleFolder.add(blocks.stone.scale, 'y', 10, 100).name('Y Scale')
-  scaleFolder.add(blocks.stone.scale, 'z', 10, 100).name('Z Scale')
+  resources.forEach(resource => {
+    const resourceFolder = resourcesFolder.addFolder(resource.name)
+    resourceFolder.add(resource, 'scarcity', 0, 1).name('Scarcity')
+
+    const scaleFolder = resourceFolder.addFolder('Scale')
+    scaleFolder.add(resource.scale, 'x', 10, 100).name('X Scale')
+    scaleFolder.add(resource.scale, 'y', 10, 100).name('Y Scale')
+    scaleFolder.add(resource.scale, 'z', 10, 100).name('Z Scale')
+  })
 
   gui.onChange(() => {
     world.generate()
